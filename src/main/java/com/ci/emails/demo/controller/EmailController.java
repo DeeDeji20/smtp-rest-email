7024c3e4eb3d09bc5fcce2e74da3d75d5e1ed373
Adeola Oladeji <deolaoladeji@gmail.com>
2022-08-31 10:52:46 +0100

    Refactoring code

--- src/main/java/com/ci/emails/demo/controller/EmailController.java
+++ src/main/java/com/ci/emails/demo/controller/EmailController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
     @Autowired
-    EmailService emailService;
+    private EmailService emailService;
 
     @PostMapping("/send")
     public String sendMessage(@RequestBody EmailDetails emailDetails){
